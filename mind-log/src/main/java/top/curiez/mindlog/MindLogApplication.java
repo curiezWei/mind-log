b4b3d4bf659ed7df16d8551a34b5b413a5774424
curiez <weihaoqi@hotmail.com>
2026-06-01 22:00:12 +0800

    start after 6.4 , cuz examination is coming...

---
+++ mind-log/src/main/java/top/curiez/mindlog/MindLogApplication.java
@@ -0,0 +1,12 @@
+package top.curiez.mindlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindLogApplication.class, args);
+    }
+}
